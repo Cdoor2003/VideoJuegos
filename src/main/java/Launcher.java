3dd0a90b749c8f21e4c7a4340f9bf3959238a9ca
Christian <c.ortega12@ufromail.cl>
2023-04-06 01:04:40 -0400

    Ultimo commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args){
+        inicializar();
+    }
+    public static void inicializar(){
+        Scanner teclado = new Scanner(System.in);
+        Arena arena = new Arena();
+        System.out.println("Ingresa el nombre de tu personaje: ");
+        String nombre = teclado.next();
+        System.out.println("Ingresa la fuerza de tu personaje(20-100): ");
+        int fuerza = teclado.nextInt();
+        System.out.println("Ingresa la velocidad de tu personaje(20-100): ");
+        int velocidad = teclado.nextInt();
+        System.out.println("Ingresa la vida de tu personaje(20-100): ");
+        int vida = teclado.nextInt();
+        System.out.println("Ingresa el nombre del ataque de tu personaje: ");
+        String ataque = teclado.next();
+        Personaje personaje = new Personaje(nombre,fuerza,velocidad,vida,ataque);
+        arena.menuPrincipal(personaje);
+    }
+}
